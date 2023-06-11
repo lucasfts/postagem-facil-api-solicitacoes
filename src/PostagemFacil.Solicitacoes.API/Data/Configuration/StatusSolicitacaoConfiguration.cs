740fb81bc64f17cf0f6e0c6f1ec92ad02ee66f65
Lucas Silva <silva.lucasfeitoza@gmail.com>
2023-06-11 19:01:32 -0300

    Populando tabela de status

---
+++ src/PostagemFacil.Solicitacoes.API/Data/Configuration/StatusSolicitacaoConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PostagemFacil.Solicitacoes.API.Data.Models;
+
+namespace PostagemFacil.Solicitacoes.API.Data.Configuration
+{
+    public class StatusSolicitacaoConfiguration : IEntityTypeConfiguration<StatusSolicitacao>
+    {
+        public void Configure(EntityTypeBuilder<StatusSolicitacao> builder)
+        {
+            builder.HasData(
+                new StatusSolicitacao() { Id = 1, Descricao = "Pagamento Pendente" },
+                new StatusSolicitacao() { Id = 2, Descricao = "Aguardando Coleta" },
+                new StatusSolicitacao() { Id = 3, Descricao = "Coletado" },
+                new StatusSolicitacao() { Id = 4, Descricao = "Preparação para postagem" },
+                new StatusSolicitacao() { Id = 5, Descricao = "Postado" }
+            );
+        }
+    }
+}
---
+++ src/PostagemFacil.Solicitacoes.API/Data/Migrations/20230611142449_SeedStatusSolicitacao.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace PostagemFacil.Solicitacoes.API.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedStatusSolicitacao : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "StatusSolicitacao",
+                columns: new[] { "Id", "Descricao" },
+                values: new object[,]
+                {
+                    { 1, "Pagamento Pendente" },
+                    { 2, "Aguardando Coleta" },
+                    { 3, "Coletado" },
+                    { 4, "Em Preparação" },
+                    { 5, "Postado" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "StatusSolicitacao",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "StatusSolicitacao",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "StatusSolicitacao",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "StatusSolicitacao",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "StatusSolicitacao",
+                keyColumn: "Id",
+                keyValue: 5);
+        }
+    }
+}
